ce8e7bae984ad00cce84a5dbbb634ef924fb4ff2
k-ms1998 <k_ms1998@naver.com>
2021-11-11 13:46:27 +0900

    Added ability to download saledata depending on the time

--- routes/hmpmall/saleData/controller.js
+++ routes/hmpmall/saleData/controller.js
@@ -13,14 +13,45 @@ const keys={
 }
 
 exports.getData = (async(req, res) => {
-    if(!req.session.hmp_cookies){
-        //307 Temporary Redirect; Will preserve the send method
-        await res.redirect(307, "login")
+    // 09시 마감 => 전날 16:00~오늘 09:00
+    // 11시 마감 => 오늘 09:00~오늘 11:00
+    // 14시 마감 => 오늘 11:00~오늘 14:00
+    
+    
+    if(!req.session.hmp_cookies || !req.body.time){
+        if(!req.body.time){
+            res.status(400).json({
+                "Error": "Please input the time"
+            })
+        }
+        else{
+            //307 Temporary Redirect; Will preserve the send method
+            await res.redirect(307, "login")
+        }
     }
     else{
+        let ts = Date.now(); //Date.now()는 UTC 기준; 한국은 UTC+9hours; 9 hours == 32400000 ms
+        let dateObj = new Date(ts);
+
+        var year = dateObj.getFullYear();
+        var month = dateObj.getMonth()+1;
+        var day = dateObj.getDate();
+
+        var dTime = req.body.time
+        var sTime = 0
+        if(dTime == '09'){
+            sTime = '14'
+        }
+        else if(dTime == '11'){
+            sTime = '09'
+        }
+        else if(dTime == '14'){
+            sTime = '11'
+        }
+
         //console.log(req.session.hmp_cookies)
         const browser = await puppeteer.launch({
-            headless: true
+            headless: false
         });
         const page = await browser.newPage();
         await page._client.send("Page.setDownloadBehavior", {
@@ -34,8 +65,18 @@ exports.getData = (async(req, res) => {
         //Move to website
         await page.goto(keys.address);
         await page.select("#productSearchType", "PRODUCT_NAME");
-        await page.type("#productSearchTypeVal", "혈당측정지");
+        await page.type("#productSearchTypeVal", req.body.search_keyW);
         await page.click("[class='per']");
+        if(dTime == 9){
+            //The order date changes back to the default value when page.select("#productSearchType", "PRODUCT_NAME"); is triggered
+            //=>Therefore, change the date after page.select("#productSearchType", "PRODUCT_NAME");
+            let orderDate = await page.$('#fromOrderDate')
+            await orderDate.click({clickCount:3})
+            await orderDate.type(year+'-'+month+'-'+(day-1))
+        
+        }
+        await page.select('#fromOrderTime', sTime);
+        await page.select('#toOrderTime', dTime);
         await page.click("#orderProductTab");
 
         await page.click("[class='srch_btn']")
@@ -55,11 +96,12 @@ exports.getData = (async(req, res) => {
                     var filenameSplit = filename.split(".");
                     var file_extension = filenameSplit[filenameSplit.length-1]
 
-                    let ts = Date.now(); //Date.now()는 UTC 기준; 한국은 UTC+9hours; 9 hours == 32400000 ms
-                    let dateObj = new Date(ts);
-
-                    var newFileName = dateObj.getFullYear()+'_'+(dateObj.getMonth()+1)+'_'+dateObj.getDate()
-
+                    var keyW = req.body.search_keyW
+                    if(keyW == ""){
+                        keyW="전체"
+                    }
+                    var newFileName = year+'_'+month+'_'+day+'_'+keyW+'_'+dTime
+                    
                     fs.rename(__downloadDir+'/'+filename, __downloadDir+'/'+ newFileName+'.'+file_extension, async(err) => {
                         if(err)
                             console.log(err)
